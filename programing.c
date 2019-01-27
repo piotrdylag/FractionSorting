8525ec63ed621671af3479b5471264aae2be4f67
Piotr <dylag.piotr@gmail.com>
2019-01-27 13:33:54 +0100

    sortowanie bąbelkowe dla ułamków

--- programing.c
+++ programing.c
@@ -1,28 +1,30 @@
-#include <stdio.h>
+#include <stdio.h>    // Instrukcja Preprocesora
 
-struct fractions{
+struct fraction {
     int counter;
     unsigned denominator;
+    float value;
 };
 
-void printArray (int *, int);
+void printArray(int *, int);
 
-int minFractions (struct fractions f1, struct fractions f2) {
-     return f1.counter * f2.denominator < f2.counter * f1.denominator;
+int minFractions(struct fraction f1, struct fraction f2) {
+    return f1.counter * f2.denominator < f2.counter * f1.denominator;
 }
 
-int printFractions(struct fractions f) {
-    printf("%d/%u \n", f.counter, f.denominator);
+int printFractions(struct fraction f) {
+    printf("%d/%u, %f \n", f.counter, f.denominator, f.value);
 }
 
-     void selectionSort(int *t, int n){                  //Sortowanie prze wybieranie
-    for(int i =0; i < n; i++){
+void selectionSort(int *t, int n) {                  //Sortowanie prze wybieranie
+    for (int i = 0; i < n; i++) {
         int y = i;
         int min = t[y];
-        for(int j = i + 1; j < n; j++){
-            if(t[j] < min){
+        for (int j = i + 1; j < n; j++) {
+            if (t[j] < min) {
                 min = t[j];
                 y = j;
+
             }
             t[y] = t[i];
             t[i] = min;
@@ -31,14 +33,14 @@ int printFractions(struct fractions f) {
     printArray(t, n);
 }
 
-void sortFract (struct fractions *t, int n){
+void sortFract(struct fraction *t, int n) {
 
-    for(int i =0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         int y = i;
-        struct fractions min = t[y];
-        for(int j = i + 1; j < n; j++){
+        struct fraction min = t[y];
+        for (int j = i + 1; j < n; j++) {
             minFractions(t[j], min);
-            if(minFractions (t[j], min) == 1) {
+            if (minFractions(t[j], min) == 1) {
                 t[y] = t[i];
                 t[i] = min;
                 min = t[j];
@@ -48,11 +50,30 @@ void sortFract (struct fractions *t, int n){
 
 }
 
+void swap(struct fraction *a, struct fraction *b) {
+    struct fraction tmp = *a;
+    *a = *b;
+    *b = tmp;
 
-void printArray(int array[], int n){
+}
+
+void bubbleSort(struct fraction *a, int n) {
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j].value > a[j + 1].value) {
+                swap(&a[j], &a[j + 1]);
+            }
+        }
+    }
+
+}
+
+
+void printArray(int array[], int n) {
     printf("{");
-    for(int i =0; i < n; i++){
-        if(i == n - 1){
+    for (int i = 0; i < n; i++) {
+        if (i == n - 1) {
             printf("%d", array[i]);
         } else {
             printf("%d, ", array[i]);
@@ -63,10 +84,21 @@ void printArray(int array[], int n){
 
 
 int main() {
+    const int n = 5;
 
-    struct fractions array[4] = {{3, 4}, {5, 8}, {1, 2}, {3, 8}};
-    sortFract(array, 4);
-    for(int i = 0; i < 4; i++){
+    struct fraction array[5] = {{5, 4},
+                                {3, 4},
+                                {5, 8},
+                                {1, 2},
+                                {3, 8},
+                                {8, 9}};
+    for (int i = 0; i < n; i++) {
+        array[i].value = (float)array[i].counter / array[i].denominator;
+        printf("%d/%d %f \n", array[i].counter, array[i].denominator, array[i].value);
+    }
+    printf("\n Sortowanie \n");
+    bubbleSort(array, n);
+    for (int i = 0; i < n; i++) {
         printFractions(array[i]);
     }
 
